12e62ecc1e9bb515e270a5da1871f7269c430c92
Chris Marais <chrismarais@whereismytransport.com>
2021-12-06 15:28:16 +0200

    day 6 part 2

--- day6.py
+++ day6.py
@@ -1,16 +1,21 @@
 from helpers import read_input, begin_part_one, begin_part_two, solution
 import numpy as np
 
-n_days = 80
 
-
-def next_state(fish: np.ndarray):
+def next_fish(fish: np.ndarray):
     n_births = np.sum(fish == 0)
     next = (fish - 1) % 7
     # Correct the 8 -> 7 transition that is incorrect
     for i in np.where(fish == 8)[0]:
         next[i] = 7
     return np.append(next, 8 * np.ones(n_births, dtype=int))
+
+
+def next_state(state: np.ndarray):
+    next = np.roll(state, -1)
+    # 0 day fish go to day 6, joining those that came from day 7
+    next[6] = next[6] + state[0]
+    return next
 
 
 fish = np.array(
@@ -21,9 +26,16 @@ fish = np.array(
 )
 
 begin_part_one()
+n_days = 80
+fish_state = fish
 for i in range(n_days):
-    fish = next_state(fish)
-print(f"Final state: {fish}")
-solution(len(fish))
+    fish_state = next_fish(fish)
+print(f"Final state: {fish_state}")
+solution(len(fish_state))
 begin_part_two()
-solution()
+n_days = 256
+# The state defines the number of fish in state i at index i
+state = np.array([np.sum(fish == i) for i in range(9)], dtype=float)
+for i in range(n_days):
+    state = next_state(state)
+solution(int(np.sum(state)))
