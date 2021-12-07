387001528444f24d568be1e26b778e188d55352f
Chris Marais <chrismarais@whereismytransport.com>
2021-12-07 20:28:05 +0200

    part 2 day 7

--- day7.py
+++ day7.py
@@ -3,13 +3,15 @@ import numpy as np
 
 
 def compute_fuel(positions, point):
-    return np.sum(np.abs(positions - point))
+    fuel = [np.sum(range(1, x + 1)) for x in np.abs(positions - point)]
+    return np.sum(fuel)
 
 
-def solve(positions: np.ndarray, radius=500):
+def solve(positions: np.ndarray, radius=10):
     mean = int(np.round(np.mean(positions)))
     print("mean =", mean)
     min_fuel = compute_fuel(positions, mean)
+    print(min_fuel)
     for i in range(radius):
         if i < mean:
             fuel_left = compute_fuel(positions, mean - i)
@@ -18,6 +20,7 @@ def solve(positions: np.ndarray, radius=500):
         fuel_right = compute_fuel(positions, mean + i)
         if fuel_right < min_fuel:
             min_fuel = fuel_right
+        print(min_fuel)
     return min_fuel
 
 
@@ -26,6 +29,6 @@ crabmarines = np.array([int(x) for x in input.split(",")], dtype=int)
 # crabmarines = np.array([16,1,2,0,4,2,7,1,2,14], dtype=int)
 
 begin_part_one()
-solution(solve(crabmarines))
 begin_part_two()
+solution(solve(crabmarines))
 solution()
