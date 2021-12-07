775dc07330f3be7bc1fc7a2facb2716de1192f13
Chris Marais <chrismarais@whereismytransport.com>
2021-12-07 20:35:35 +0200

    solve both

--- day7.py
+++ day7.py
@@ -2,33 +2,31 @@ from helpers import read_input, begin_part_one, begin_part_two, solution
 import numpy as np
 
 
-def compute_fuel(positions, point):
-    fuel = [np.sum(range(1, x + 1)) for x in np.abs(positions - point)]
+def compute_fuel(positions, point, ramping):
+    fuel = np.abs(positions - point)
+    if ramping:
+        fuel = [np.sum(range(1, x + 1)) for x in fuel]
     return np.sum(fuel)
 
 
-def solve(positions: np.ndarray, radius=10):
+def solve(positions: np.ndarray, radius=10, ramping=False):
     mean = int(np.round(np.mean(positions)))
-    print("mean =", mean)
-    min_fuel = compute_fuel(positions, mean)
-    print(min_fuel)
+    min_fuel = compute_fuel(positions, mean, ramping)
     for i in range(radius):
         if i < mean:
-            fuel_left = compute_fuel(positions, mean - i)
+            fuel_left = compute_fuel(positions, mean - i, ramping)
             if fuel_left < min_fuel:
                 min_fuel = fuel_left
-        fuel_right = compute_fuel(positions, mean + i)
+        fuel_right = compute_fuel(positions, mean + i, ramping)
         if fuel_right < min_fuel:
             min_fuel = fuel_right
-        print(min_fuel)
     return min_fuel
 
 
 input = read_input("./inputs/day7.txt", splitlines=False)
 crabmarines = np.array([int(x) for x in input.split(",")], dtype=int)
-# crabmarines = np.array([16,1,2,0,4,2,7,1,2,14], dtype=int)
 
 begin_part_one()
+solution(solve(crabmarines, radius=500))
 begin_part_two()
-solution(solve(crabmarines))
-solution()
+solution(solve(crabmarines, ramping=True))
