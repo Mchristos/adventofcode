f3c012e2e6a35b50ed15ec8b32dc3da72875456d
Chris Marais <chrismarais@whereismytransport.com>
2021-12-11 14:46:46 +0200

    remove invalid letters logic!

--- day11.py
+++ day11.py
@@ -1,6 +1,7 @@
 from helpers import read_input, begin_part_one, begin_part_two, solution
 
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+# exclude "i", "o", "l"
+ALPHABET = "abcdefghjkmnpqrstuvwxyz"
 
 
 def increment_at(password, i):
@@ -10,16 +11,6 @@ def increment_at(password, i):
         return increment_at(new_password, i - 1)
     else:
         return new_password
-
-
-def force_increment_at(password, index):
-    i_next = (ALPHABET.index(password[index]) + 1) % len(ALPHABET)
-    new_password = list(password)
-    new_password[index] = ALPHABET[i_next]
-    if index < len(password) - 1:
-        new_password[index + 1 :] = list("a" * (len(password) - index - 1))
-    new_password = "".join(new_password)
-    return new_password
 
 
 def increment(password):
@@ -38,22 +29,6 @@ def includes_straight(password: str):
     return False
 
 
-def contains_bad_chars(password: str):
-    return ("i" in password) or ("o" in password) or ("l" in password)
-
-
-def find_bad_char(password: str):
-    min = 999
-    for bad in ["i", "o", "l"]:
-        try:
-            bad_i = password.index(bad)
-            if bad_i < min:
-                min = bad_i
-        except ValueError:
-            continue
-    return min
-
-
 def contains_two_pairs(password: str):
     pairs = []
     for i in range(1, len(password)):
@@ -61,28 +36,20 @@ def contains_two_pairs(password: str):
             if password[i] not in pairs:
                 pairs.append(password[i])
     if len(pairs) >= 2:
-        # print(f"Found pairs {pairs} in {password}")
         return True
     else:
         return False
 
 
 def is_valid(password: str):
-    return (
-        includes_straight(password)
-        and contains_two_pairs(password)
-        and not contains_bad_chars(password)
-    )
+    return includes_straight(password) and contains_two_pairs(password)
 
 
 def solve(input: str):
     valid = False
     password = input
     while not valid:
-        if contains_bad_chars(password):
-            password = force_increment_at(password, find_bad_char(password))
-        else:
-            password = increment(password)
+        password = increment(password)
         valid = is_valid(password)
     return password
 
