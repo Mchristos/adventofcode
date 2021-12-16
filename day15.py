8f961d8f4061a93a8a2a53a7448c56c6f9fdf6dc
Chris Marais <chrismarais@whereismytransport.com>
2021-12-16 09:14:58 +0200

    dont recompute scores every time

--- day15.py
+++ day15.py
@@ -4,48 +4,19 @@ from functools import reduce
 import numpy as np
 
 
-def find_adj(i, length):
-    i_vals = []
-    if i > 0:
-        i_vals.append(i - 1)
-    if i < length - 1:
-        i_vals.append(i + 1)
-    return i_vals
-
-
-def find_adjacent(position, shape):
-    i, j = position
-    i_vals = find_adj(i, shape[0])
-    j_vals = find_adj(j, shape[1])
-    return [[i, j_] for j_ in j_vals] + [[i_, j] for i_ in i_vals]
-
-
-def compute_score(graph, path):
-    score = 0
-    for pos in path:
-        score += graph[pos[0], pos[1]]
-    return score
-
-
-def expand_path(graph, path, visited):
-    adjacents = find_adjacent(path[-1], graph.shape)
-    new_visits = [next for next in adjacents if next not in visited]
-    return [path + [next] for next in new_visits], new_visits
-
-
-def print_path(path, shape):
-    picture = np.zeros(shape)
-    for pos in path:
-        picture[tuple(pos)] = 1
-    print(picture)
-
-
 def mad_multiplier(array, axis, times=5, loop=9):
     """
     Creates multiple copies of an array, stacked along the given
     axis (axis = 0 stacks new copies below, axis=1 stacks them to the right).
     Each is a copy of the original array plus one at each entry, where after
-    9 (loop) you loop back to 1.
+    <loop> (default 9) you loop back to 1.
+
+    axis : 0 or 1. If 0, multiplies the array "downwards" i.e. along the 0 direction.
+           If 1, multiplies to the right.
+    times: Number of array copies in the result
+    loop : Value after which numbers loop back to 1
+
+    Returns a new array with dimensions [array.shape[0]*times, array.shape[1]] if axis = 0.
 
     e.g. if array = [[1,2]
                      [3,4]]
@@ -64,26 +35,74 @@ def mad_multiplier(array, axis, times=5, loop=9):
     )
 
 
+def find_adj(i, length):
+    i_vals = []
+    if i > 0:
+        i_vals.append(i - 1)
+    if i < length - 1:
+        i_vals.append(i + 1)
+    return i_vals
+
+
+def find_adjacent(position: tuple[int], shape: tuple[int]) -> list[tuple[int]]:
+    i, j = position
+    i_vals = find_adj(i, shape[0])
+    j_vals = find_adj(j, shape[1])
+    return [(i, j_) for j_ in j_vals] + [(i_, j) for i_ in i_vals]
+
+
+def compute_score(graph, path):
+    score = 0
+    for pos in path:
+        score += graph[pos[0], pos[1]]
+    return score
+
+
+def expand_path(
+    graph: np.ndarray, path: list[tuple[int]], score: int, visited: dict[tuple, bool]
+):
+    adjacents = find_adjacent(path[-1], graph.shape)
+    new_visits = [next for next in adjacents if next not in visited]
+    new_paths = [path + [next] for next in new_visits]
+    new_scores = [score + graph[next] for next in new_visits]
+    return new_paths, new_scores
+
+
+def print_path(path, shape):
+    picture = np.zeros(shape)
+    for pos in path:
+        picture[tuple(pos)] = 1
+    print(picture)
+
+
+def solve(graph):
+    paths = [[(0, 0)]]
+    scores = [0]
+    visited = {(0, 0): True}
+    destination = (graph.shape[0] - 1, graph.shape[1] - 1)
+    while True:
+        # # expand the least score path
+        # scores = [compute_score(graph, path) for path in paths]
+        i_least_path = scores.index(min(scores))
+        new_paths, new_scores = expand_path(
+            graph, paths[i_least_path], scores[i_least_path], visited
+        )
+        # check for solution found
+        for path in new_paths:
+            visited[path[-1]] = True
+            if path[-1] == destination:
+                return path
+        # if not expand the set of paths and visits
+        paths = paths[:i_least_path] + paths[i_least_path + 1 :] + new_paths
+        scores = scores[:i_least_path] + scores[i_least_path + 1 :] + new_scores
+
+
 input = read_input("./inputs/day15.txt")
 graph = np.array([[int(x) for x in line] for line in input])
 
 
 begin_part_one()
-paths = [[[0, 0]]]
-visited = [[0, 0]]
-distination = [graph.shape[0] - 1, graph.shape[1] - 1]
-while True:
-    # expand path with least score
-    scores = [compute_score(graph, path) for path in paths]
-    i_least_path = scores.index(min(scores))
-    least_path = paths[i_least_path]
-    if least_path[-1] == distination:
-        break
-    else:
-        new_paths, new_visits = expand_path(graph, least_path, visited)
-        paths = paths[:i_least_path] + paths[i_least_path + 1 :] + new_paths
-        visited += new_visits
-
+least_path = solve(graph)
 print_path(least_path, graph.shape)
 solution(compute_score(graph, least_path[1:]))
 
